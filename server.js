4210d00a002d09f73b980087b2068c2373f64b71
Vijay <asp.vijaybaskar@gmail.com>
2018-10-06 01:05:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,29 +20,29 @@ app.post('/shops',function(request,response){
     const filteredList = shops.NewYork.filter(function(title) {
         return title.shopname==shopname;
       });
-     
-      var resultObject={
-        speech:filteredList[0].shopname,
-        displayText:filteredList[0].shopname,
-        resourse:'simpleresponse'
-      }
+     const all = shops.NewYork.filter(function(title) {
+        return title;
+      });  
+      var numberofobjects=Object.keys(shops.NewYork).length;   
+
+var loop = [];
+
+for(var x = 0; x < numberofobjects; x++){
+ loop.push(
+    {
+        "card": {
+          "title": all[x].shopname,
+          "address": all[x].address,
+          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+          
+        }
+    }
+      
     
-  var full={
-        "fulfillmentMessages": [
-            {
-              "card": {
-                "title": filteredList[0].shopname,
-                "subtitle": "sample title from service",
-                "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                "buttons": [
-                  {
-                    "text": "button text",
-                    "postback": "https://assistant.google.com/"
-                  }
-                ]
-              }
-            }
-          ]}
+ );
+}
+var full={"fulfillmentMessages": loop}
+          
     return response.send(full);
 
 
