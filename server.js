aeb345c05b8a4e91c064af65e3c14d6c1ee81f0e
Vijay <asp.vijaybaskar@gmail.com>
2018-10-09 19:14:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyparser = require('body-parser');
 const shops = require('./speedwayShops.json');
@@ -14,176 +13,199 @@ app.get('/', function (request, response) {
     response.send('The application is running');
 });
 
+
+
+
 app.post('/shops', function (request, response) {
 
-    if (request.body.queryResult.action == "action_list_Items") {
+    switch (request.body.queryResult.action) {
+        case "action_list_products":
+            ///actions list items starts here
 
-        var numberofobjects = Object.keys(inventory.cooking_essentials).length;
-        var rowData = [];
-        for (var x = 0; x < numberofobjects; x++) {
-            rowData.push(
-                {
-                    "cells": [
-                        {
-                            "text": inventory.cooking_essentials[x].Productname
-                        },
-                        {
-                            "text": inventory.cooking_essentials[x].Price
-
-                        },
-                        {
-                            "buttons": [{
-                                "title": "Add",
-                                "openUrlAction": {
-                                                "url": "https://github.com/actions-on-google"
-                                            }
-                            }]
-                        }
-                    ],
-                    "dividerAfter": true
-                }
-            );
-        }
-
-        const fullfilmentResponse = {
-            "fulfillmentText": "here the list of items in this shop",
-            "payload": {
-                "google": {
-                    "expectUserResponse": true,
-                    "richResponse": {
-                        "items": [
+            var numberofobjects = Object.keys(inventory.cooking_essentials).length;
+            var rowData = [];
+            for (var x = 0; x < numberofobjects; x++) {
+                rowData.push(
+                    {
+                        "cells": [
                             {
-                                "simpleResponse": {
-                                    "textToSpeech": "Simple Response"
-                                }
+                                "text": inventory.cooking_essentials[x].Productname
                             },
                             {
-                                "tableCard": {
-                                    "title": "List of Products ",
-                                    "subtitle": "",
-                                    "image": {
-                                        "url": "https://avatars0.githubusercontent.com/u/23533486",
-                                        "accessibilityText": "Actions on Google"
-                                    },
-                                    "rows": rowData,
-                                    "columnProperties": [
-                                        {
-                                            "header": "Product Name"
-                                            
-                                        },
-                                        {
-                                            "header": "Price"
-                                            
-                                        },
-                                         {
-                                            "header": "Quantity"
-                                            
-                                        }
-                                    ],
-                                    "buttons": [
-                                        {
-                                            "title": "Check out",
-                                            "openUrlAction": {
-                                                "url": "https://github.com/actions-on-google"
-                                            }
-                                        }
-                                    ]
-                                }
+                                "text": inventory.cooking_essentials[x].Price
+
+                            },
+                            {
+                                "buttons": [{
+                                    "text": "Add",
+                                    "postback": ""
+                                }]
                             }
-                        ]
-                    },
-                    "userStorage": "{\"data\":{}}"
-                }
+                        ],
+                        "dividerAfter": true
+                    }
+                );
             }
-
-        }
-
-        return response.send(fullfilmentResponse);
-    }
-
-    else if(request.body.queryResult.action == "action_search_shops") {
-        const shopname = "Speedway Brooklyn 11207"
-        const filteredList = shops.NewYork.filter(function (title) {
-            return title.shopname == shopname;
-        });
-        const all = shops.NewYork.filter(function (title) {
-            return title;
-        });
-        var numberofobjects = Object.keys(shops.NewYork).length;
-
-        var loop = [];
-        for (var x = 0; x < numberofobjects; x++) {
-            loop.push(
-                {
-                    "card": {
-                        "title": all[x].shopname,
-                        "address": all[x].shopname,
-                        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-
+            const fullfilmentResponse = {
+                "fulfillmentText": "here the list of items in this shop",
+                "payload": {
+                    "google": {
+                        "expectUserResponse": true,
+                        "richResponse": {
+                            "items": [
+                                {
+                                    "simpleResponse": {
+                                        "textToSpeech": "Simple Response"
+                                    }
+                                },
+                                {
+                                    "tableCard": {
+                                        "title": "List of Products ",
+                                        "subtitle": "",
+                                        "image": {
+                                            "url": "https://avatars0.githubusercontent.com/u/23533486",
+                                            "accessibilityText": "Actions on Google"
+                                        },
+                                        "rows": rowData,
+                                        "columnProperties": [
+                                            {
+                                                "header": "Product Name",
+                                                "horizontalAlignment": "CENTER"
+                                            },
+                                            {
+                                                "header": "Quantity",
+                                                "horizontalAlignment": "LEADING"
+                                            },
+                                            {
+                                                "header": "Price",
+                                                "horizontalAlignment": "TRAILING"
+                                            }
+                                        ],
+                                        "buttons": [
+                                            {
+                                                "title": "Check out",
+                                                "openUrlAction": {
+                                                    "url": "https://github.com/actions-on-google"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            ]
+                        },
+                        "userStorage": "{\"data\":{}}"
                     }
                 }
 
-
-            );
-        }
-          
- var full = {
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "Choose a item"
             }
-          }
-        ]
-      },
-      "systemIntent": {
-        "intent": "actions.intent.OPTION",
-        "data": {
-          "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-          "listSelect": {
-            "title": "Hello",
-            "items": [
-              {
-                "optionInfo": {
-                  "key": "first title key"
-                },
-                "description": "first description",
-                "image": {
-                  "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-                  "accessibilityText": "first alt"
-                },
-                "title": "first title"
-              },
-              {
-                "optionInfo": {
-                  "key": "second"
-                },
-                "description": "second description",
-                "image": {
-                  "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-                  "accessibilityText": "second alt"
-                },
-                "title": "second title"
-              }
-            ]
-          }
-        }
-      }
-    }
-  }
-}
-        return response.send(full);
-    }
-    return response.send("The application is excited");
-});
+
+            return response.send(fullfilmentResponse);
+
+            break;
+
+        /////End here 
+
+
+        case "action_search_shops":
+            //// action_list_categories starts here
+
+            const shopname = "Speedway Brooklyn 11207"
+            const filteredList = shops.NewYork.filter(function (title) {
+                return title.shopname == shopname;
+            });
+
+            const all = shops.NewYork.filter(function (title) {
+                return title;
+            });
+            var numberofobjects = Object.keys(shops.NewYork).length;
+
+            var loop = [];
+
+            // var jsonobject=JSON.parse(shops.NewYork);
+
+            for (var x = 0; x < numberofobjects; x++) {
+                loop.push(
+                    {
+                        "card": {
+                            "title": all[x].shopname,
+                            "address": all[x].shopname,
+                            "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+
+                        }
+                    }
+
+
+                );
+            }
+
+
+            return response.send(loop);
+
+
+            ////Ends
+            break;
+
+        case "action_list_categories":
+            //// action_list_categories starts here
+            var numberofobjects = Object.keys(inventory.Productcategories).length;
+            var listData = [];
+
+            for (var x = 0; x < numberofobjects; x++) {
+                listData.push(
+                    {
+                        "optionInfo": {
+                            "key": "ListKey_" + x
+                        },
+                        "description": inventory.Productcategories[x].description,
+                        "image": {
+                            "url": inventory.Productcategories[x].url,
+                            "accessibilityText": inventory.Productcategories[x].productcategory
+                        },
+                        "title": inventory.Productcategories[x].productcategory
+                    }
+                );
+            }
+
+
+
+            var listFullfillment = {
+                "payload": {
+                    "google": {
+                        "expectUserResponse": true,
+                        "richResponse": {
+                            "items": [
+                                {
+                                    "simpleResponse": {
+                                        "textToSpeech": "Choose a item"
+                                    }
+                                }
+                            ]
+                        },
+                        "systemIntent": {
+                            "intent": "actions.intent.OPTION",
+                            "data": {
+                                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                                "listSelect": {
+                                    "title": "Please select the category",
+                                    "items": listData
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return response.send(listFullfillment);
+            ////Ends here
+            break;
+        default:
+            /// Default case 
+            break;
+    } /// End of Switch Statement
+}); /// End of POST method
 
 
 
 app.listen(listeningPort, function () {
     console.log('The application in Port ...' + listeningPort);
 });
-
