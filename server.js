5d34f2aeebca32ad6b79aaec5cfc862ba4be5063
Vijay <asp.vijaybaskar@gmail.com>
2018-10-09 16:07:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -124,129 +124,126 @@ app.post('/shops', function (request, response) {
 
             );
         }
-          var full= {
-  "expectUserResponse": true,
-  "expectedInputs": [
-    {
-      "inputPrompt": {
-        "richInitialPrompt": {
-          "items": [
-            {
-              "simpleResponse": {
-                "textToSpeech": "This is a simple response for a list."
-              }
-            }
-          ],
-          "suggestions": [
-            {
-              "title": "Basic Card"
-            },
-            {
-              "title": "Browse Carousel"
-            },
-            {
-              "title": "Carousel"
-            },
-            {
-              "title": "List"
-            },
-            {
-              "title": "Media"
-            },
-            {
-              "title": "Suggestions"
-            }
-          ]
-        }
-      },
-      "possibleIntents": [
-        {
-          "intent": "actions.intent.OPTION",
-          "inputValueData": {
-            "listSelect": {
-              "title": "List Title",
-              "items": [
+          
+ var full = {
+    "fulfillmentText": "here the list of items in this shop",
+    "payload": {
+        "google": {
+            "conversationToken": "",
+            "expectUserResponse": true,
+            "expectedInputs": [
                 {
-                  "image": {
-                    "accessibilityText": "Image alternate text",
-                    "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png"
-                  },
-                  "optionInfo": {
-                    "synonyms": [
-                      "synonym of title 1",
-                      "synonym of title 2",
-                      "synonym of title 3"
-                    ],
-                    "key": "title"
-                  },
-                  "description": "This is a description of a list item.",
-                  "title": "Title of First List Item"
-                },
-                {
-                  "image": {
-                    "accessibilityText": "Google Home",
-                    "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw"
-                  },
-                  "optionInfo": {
-                    "synonyms": [
-                      "Google Home Assistant",
-                      "Assistant on the Google Home"
-                    ],
-                    "key": "Speed way shop 1"
-                  },
-                  "description": "Google Home is a voice-activated speaker powered by the Google Assistant.",
-                  "title": "Google Home"
-                },
-                {
-                  "image": {
-                    "accessibilityText": "Google Pixel",
-                    "url": "https://storage.googleapis.com/madebygoog/v1/Pixel/Pixel_ColorPicker/Pixel_Device_Angled_Black-720w.png"
-                  },
-                  "optionInfo": {
-                    "synonyms": [
-                      "Google Pixel XL",
-                      "Pixel",
-                      "Pixel XL"
-                    ],
-                    "key": "speed Way shop 2"
-                  },
-                  "description": "Pixel. Phone by Google.",
-                  "title": "Google Pixel"
-                },
-                {
-                  "image": {
-                    "accessibilityText": "Google Allo Logo",
-                    "url": "https://allo.google.com/images/allo-logo.png"
-                  },
-                  "optionInfo": {
-                    "synonyms": [
-                      "Allo"
-                    ],
-                    "key": "Speed Way shop 4"
-                  },
-                  "description": "Introducing Google Allo, a smart messaging app that helps you say more and do more.",
-                  "title": "Google Allo"
+                    "inputPrompt": {
+                        "richInitialPrompt": {
+                            "items": [
+                                {
+                                    "simpleResponse": {
+                                        "textToSpeech": "This is a simple response for a list."
+                                    }
+                                }
+                            ],
+                            "suggestions": [
+                                {
+                                    "title": "Basic Card"
+                                },
+                                {
+                                    "title": "Browse Carousel"
+                                },
+                                {
+                                    "title": "Carousel"
+                                },
+                                {
+                                    "title": "List"
+                                },
+                                {
+                                    "title": "Media"
+                                },
+                                {
+                                    "title": "Suggestions"
+                                }
+                            ]
+                        }
+                    },
+                    "possibleIntents": [
+                        {
+                            "intent": "actions.intent.OPTION",
+                            "inputValueData": {
+                                "listSelect": {
+                                    "title": "List Title",
+                                    "items": [
+                                        {
+                                            "image": {
+                                                "accessibilityText": "Image alternate text",
+                                                "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png"
+                                            },
+                                            "optionInfo": {
+                                                "synonyms": [
+                                                    "synonym of title 1",
+                                                    "synonym of title 2",
+                                                    "synonym of title 3"
+                                                ],
+                                                "key": "title"
+                                            },
+                                            "description": "This is a description of a list item.",
+                                            "title": "Title of First List Item"
+                                        },
+                                        {
+                                            "image": {
+                                                "accessibilityText": "Google Home",
+                                                "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw"
+                                            },
+                                            "optionInfo": {
+                                                "synonyms": [
+                                                    "Google Home Assistant",
+                                                    "Assistant on the Google Home"
+                                                ],
+                                                "key": "Speed way shop 1"
+                                            },
+                                            "description": "Google Home is a voice-activated speaker powered by the Google Assistant.",
+                                            "title": "Google Home"
+                                        },
+                                        {
+                                            "image": {
+                                                "accessibilityText": "Google Pixel",
+                                                "url": "https://storage.googleapis.com/madebygoog/v1/Pixel/Pixel_ColorPicker/Pixel_Device_Angled_Black-720w.png"
+                                            },
+                                            "optionInfo": {
+                                                "synonyms": [
+                                                    "Google Pixel XL",
+                                                    "Pixel",
+                                                    "Pixel XL"
+                                                ],
+                                                "key": "speed Way shop 2"
+                                            },
+                                            "description": "Pixel. Phone by Google.",
+                                            "title": "Google Pixel"
+                                        },
+                                        {
+                                            "image": {
+                                                "accessibilityText": "Google Allo Logo",
+                                                "url": "https://allo.google.com/images/allo-logo.png"
+                                            },
+                                            "optionInfo": {
+                                                "synonyms": [
+                                                    "Allo"
+                                                ],
+                                                "key": "Speed Way shop 4"
+                                            },
+                                            "description": "Introducing Google Allo, a smart messaging app that helps you say more and do more.",
+                                            "title": "Google Allo"
+                                        }
+                                    ]
+                                },
+                                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec"
+                            }
+                        }
+                    ]
                 }
-              ]
-            },
-            "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec"
-          }
+            ],
+            "userStorage": "{\"data\":{}}"
         }
-      ]
     }
-  ],
-  "responseMetadata": {
-    "status": {
-      "message": "Success (200)"
-    },
-    "queryMatchInfo": {
-      "queryMatched": true,
-      "intent": "16fdf55a-d1d6-48a8-8909-8b9e1f023fac"
-    }
-  },
-  "userStorage": "{\"data\":{}}"
 }
-
         return response.send(full);
     }
     return response.send("The application is excited");
