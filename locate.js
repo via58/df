5b4fd1031bfb7de8f67690915299ee72e16d3577
Vijay <asp.vijaybaskar@gmail.com>
2018-10-10 16:59:10 +0530

    Add files via upload

---
+++ locate.js
@@ -0,0 +1,46 @@
+var where = require('where');
+var shops = require("./speedwayShops.json");
+
+const all = shops.NewYork.filter(function (title) {
+    return title;
+});
+var numberofobjects = Object.keys(shops.NewYork).length;
+
+
+var data=[];
+for (var x = 0; x < numberofobjects; x++) {
+
+    var fixedLocation = new where.Point(40.707312, -74.011934); //New York Stock Exchange
+    var locationY = new where.Point(shops.NewYork[x].geo_location.latitude, shops.NewYork[x].geo_location.longitude);
+    const calulatedDistance = (fixedLocation.distanceTo(locationY));
+    data.push(
+        {
+            "calculatedTempDistanceinKm": calulatedDistance,
+            "optionInfo": {
+                "key": "carouselKey_" + x,
+                "synonyms": shops.NewYork[x].shopname
+            },
+            "title": shops.NewYork[x].shopname,
+            "description": shops.NewYork[x].address,
+            "image": {
+                "url": shops.NewYork[x].ShopImage.url,
+                "accessibilityText": shops.NewYork[x].shoptitle
+            }
+        }
+
+    );
+}
+data.sort(function(a, b) {
+    return parseFloat(a.calculatedTempDistanceinKm) - parseFloat(b.calculatedTempDistanceinKm);
+});
+
+module.exports =data;
+
+
+
+
+
+
+
+
+
