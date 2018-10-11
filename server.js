1f43fd0adc2bce2c0c4017e85adaf4e11dea3d83
Vijay <asp.vijaybaskar@gmail.com>
2018-10-11 19:43:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const inventory = require('./inventory.json');
 const request = require('request');
 const nearestshops = require('./locate.js');
 const listeningPort = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(bodyparser.json());
@@ -14,12 +15,218 @@ app.get('/', function (request, response) {
 
 app.post('/shops', function (request, response) {
 
-     const a =request.body.inputs[0].rawInputs[0].query;
-    if(a=="shops near me"){
-        return response.send({fulfillmentText="success"});
-    }
-   
-    
+//const inputName=request.body.inputs.rawInputs.query;
+
+
+
+    switch (request.body.queryResult.action) {
+        case "action_list_products":
+            ///actions list items starts here
+
+            var numberofobjects = Object.keys(inventory.cooking_essentials).length;
+            var rowData = [];
+            for (var x = 0; x < numberofobjects; x++) {
+                rowData.push(
+                    {
+                        "cells": [
+                            {
+                                "text": inventory.cooking_essentials[x].Productname
+                            },
+                            {
+                                "text": inventory.cooking_essentials[x].Price
+
+                            },
+                            {
+                                "buttons": [{
+                                    "text": "Add",
+                                    "postback": ""
+                                }]
+                            }
+                        ],
+                        "dividerAfter": true
+                    }
+                );
+            }
+            const fullfilmentResponse = {
+                "fulfillmentText": "here the list of items in this shop",
+                "payload": {
+                    "google": {
+                        "expectUserResponse": true,
+                        "richResponse": {
+                            "items": [
+                                {
+                                    "simpleResponse": {
+                                        "textToSpeech": "Simple Response"
+                                    }
+                                },
+                                {
+                                    "tableCard": {
+                                        "title": "List of Products ",
+                                        "subtitle": "",
+                                        "image": {
+                                            "url": "https://avatars0.githubusercontent.com/u/23533486",
+                                            "accessibilityText": "Actions on Google"
+                                        },
+                                        "rows": rowData,
+                                        "columnProperties": [
+                                            {
+                                                "header": "Product Name",
+                                            },
+                                            {
+                                                "header": "Price",
+                                            },
+                                            {
+                                                "header": "Quantity",
+                                            }
+                                        ],
+                                        "buttons": [
+                                            {
+                                                "title": "Check out",
+                                                "openUrlAction": {
+                                                    "url": "https://github.com/actions-on-google"
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            ]
+                        },
+                        "userStorage": "{\"data\":{}}"
+                    }
+                }
+
+            }
+
+            return response.send(fullfilmentResponse);
+
+            break;
+
+        /////End here 
+
+
+        case "action_search_shops":
+            //// action_list_categories starts here
+
+            var numberofobjects = Object.keys(shops.NewYork).length;
+
+            var carouselData = [];
+
+            // var jsonobject=JSON.parse(shops.NewYork);
+
+            for (var x = 0; x < 3; x++) {          /// Default you change the number of response
+                carouselData.push(
+                    {
+                        "optionInfo": {
+                            "key": nearestshops[x].optionInfo.key,
+                            "synonyms": nearestshops[x].optionInfo.synonyms
+                        },
+                        "title": nearestshops[x].title,
+                        "description": nearestshops[x].description,
+                        "image": {
+                            "url": nearestshops[x].image.url,
+                            "accessibilityText": nearestshops[x].image.accessibilityText
+                        }
+                    }
+
+                );
+            }
+
+            var carouselFullfillment = {
+                "payload": {
+                    "google": {
+                        "expectUserResponse": true,
+                        "richResponse": {
+                            "items": [
+                                {
+                                    "simpleResponse": {
+                                        "textToSpeech": "Choose a item"
+                                    }
+                                }
+                            ]
+                        },
+                        "systemIntent": {
+                            "intent": "actions.intent.OPTION",
+                            "data": {
+                                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                                "carouselSelect": {
+                                    "items": carouselData
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return response.send(carouselFullfillment);
+
+
+            ////Ends
+            break;
+
+        case "action_list_categories":
+            //// action_list_categories starts here
+            var numberofobjects = Object.keys(inventory.Productcategories).length;
+            var listData = [];
+
+            for (var x = 0; x < numberofobjects; x++) {
+                listData.push(
+                    {
+                        "optionInfo": {
+                            "key": "ListKey_" + x
+                        },
+                        "description": inventory.Productcategories[x].description,
+                        "image": {
+                            "url": inventory.Productcategories[x].url,
+                            "accessibilityText": inventory.Productcategories[x].productcategory
+                        },
+                        "title": inventory.Productcategories[x].productcategory
+                    }
+                );
+            }
+
+
+
+            var listFullfillment = {
+                "payload": {
+                    "google": {
+                        "expectUserResponse": true,
+                        "richResponse": {
+                            "items": [
+                                {
+                                    "simpleResponse": {
+                                        "textToSpeech": "Choose a item"
+                                    }
+                                }
+                            ]
+                        },
+                        "systemIntent": {
+                            "intent": "actions.intent.OPTION",
+                            "data": {
+                                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                                "listSelect": {
+                                    "title": "Please select the category",
+                                    "items": listData
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return response.send(listFullfillment);
+            ////Ends here
+            break;
+
+        default:
+            /// Default case 
+            break;
+    } /// End of Switch Statement for ActionName
+
+
+
+
+
+
 }); /// End of POST method
 
 
